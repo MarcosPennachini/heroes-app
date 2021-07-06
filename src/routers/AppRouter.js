91434b301bcc57bc0f9c733540b773a7831e3d2a
MarcosPenna <marcoskpo_penpun@hotmail.com>
2021-07-05 23:06:03 -0300

    Added 2nd router - DashboardRouter

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LoginScreen from '../components/login/LoginScreen';
-import { Navbar } from '../components/ui/Navbar';
-import MarvelScreen from '../components/marvel/MarvelScreen';
+import DashboardRouter from './DashboardRouter';
 
 const AppRouter = () => {
   return (
     <Router>
       <div>
-        <Navbar />
-
         <Switch>
           <Route exact path='/login' component={LoginScreen} />
-          <Route exact path='/' component={MarvelScreen} />
+          <Route path='/' component={DashboardRouter} />
         </Switch>
       </div>
     </Router>
