be817a01f23c9f162be1ccd4f5e7cbcab730b381
MarcosPenna <marcoskpo_penpun@hotmail.com>
2021-07-06 00:39:38 -0300

    Added HeroesList component to show heroes in each publisher

--- src/components/dc/DcScreen.js
+++ src/components/dc/DcScreen.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import HeroesList from '../heroes/HeroesList';
 
 const DcScreen = () => {
   return (
-    <div>
-      <h2>DC Screen</h2>
+    <div className='container-fluid mt-2'>
+      <h2>DC Heroes</h2>
+      <hr />
+      <HeroesList publisher='DC Comics' />
     </div>
   );
 };
