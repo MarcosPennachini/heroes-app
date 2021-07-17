ea047941424c47b0b6eb8eb721ff4510a0288532
MarcosPenna <marcoskpo_penpun@hotmail.com>
2021-07-16 23:22:23 -0300

    Finished action login on reducer. Added user name on navbar. Added effect to set update user on localStorage

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AUTH_LOGIN } from '../../actions/action-types';
+import { AuthContext } from '../../auth/AuthContext';
 
-const LoginScreen = () => {
+const LoginScreen = ({ history }) => {
+  const userDemo = 'Marcos';
+  const { dispatch } = useContext(AuthContext);
+
   const handleClick = () => {
-    console.log('click');
+    dispatch({
+      type: AUTH_LOGIN,
+      payload: {
+        name: userDemo,
+      },
+    });
+
+    history.replace('/');
   };
 
   return (
