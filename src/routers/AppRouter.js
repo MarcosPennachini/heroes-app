c0e32e01c7c85666cd9672af9849f3d7f8402f00
MarcosPenna <marcoskpo_penpun@hotmail.com>
2021-07-20 23:46:47 -0300

    Setting private and public routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { AuthContext } from '../auth/AuthContext';
 import LoginScreen from '../components/login/LoginScreen';
 import DashboardRouter from './DashboardRouter';
+import PrivateRoute from './PrivateRoute';
 
 const AppRouter = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <Router>
       <div>
         <Switch>
           <Route exact path='/login' component={LoginScreen} />
-          <Route path='/' component={DashboardRouter} />
+          <PrivateRoute
+            path='/'
+            isAuthenticated={user.logged}
+            component={DashboardRouter}
+          />
         </Switch>
       </div>
     </Router>
