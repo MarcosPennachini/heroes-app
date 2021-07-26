a54fcf019ae15108cbc2b30aa1528fa75d6cc0f5
MarcosPenna <marcoskpo_penpun@hotmail.com>
2021-07-25 22:16:54 -0300

    Adding vercel.json

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { AuthContext } from '../auth/AuthContext';
 import LoginScreen from '../components/login/LoginScreen';
 import DashboardRouter from './DashboardRouter';
